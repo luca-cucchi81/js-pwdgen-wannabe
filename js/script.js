3f7d071ca25d698913b8dd13433b9ba5668632c1
Luca <lu.cucchi@gmail.com>
2020-02-17 14:56:18 +0100

    aggiunta esercizi bonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ var colore = prompt ('Il tuo colore preferito?');
 
 var password = nomeUtente + cognomeUtente + colore + 20;
 
+/*ESERCIZIO BONUS 1*/
+// var data = new Date();
+// var anno = data.getFullYear();
+// var password = nomeUtente + cognomeUtente + colore + anno;
+
+/*ESERCIZIO BONUS 2*/
+// var data = new Date();
+// var anno = data.getFullYear();
+// var password = nomeUtente + ';' + cognomeUtente + ';' + colore + ';' + anno;
+
 document.getElementById('nome-utente').innerHTML = nomeUtente;
 document.getElementById('cognome-utente').innerHTML = cognomeUtente;
 document.getElementById('colore').innerHTML = colore;
